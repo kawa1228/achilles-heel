bc194542b891fffb30c0885c89383420966753dd
kawa1228 <sxa.1105bo@gmail.com>
2019-10-11 23:17:29 +0900

    layout: login　layoutを追加

---
+++ src/layouts/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app dark>
+    <v-toolbar fixed app max-height="56">
+      <v-toolbar-title>
+        <v-btn to="/" nuxt v-text="'Achilles heels'" />
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
