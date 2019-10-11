988a04c71fcd7a1bd650b6b1b65db50a6d5929d9
kawa1228 <sxa.1105bo@gmail.com>
2019-10-11 23:18:02 +0900

    fix: ログインページ修正

--- src/pages/admin.vue
+++ src/pages/admin.vue
@@ -1,11 +1,23 @@
 <template>
-  <button @click="signIn">googleログイン</button>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title class="headline">
+          ログイン
+        </v-card-title>
+        <v-card-text>
+          <button @click="signIn">googleログイン</button>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 import { firebaseLogin } from '~/plugins/firebase'
 
 export default {
+  layout: 'login',
   methods: {
     async signIn() {
       try {
