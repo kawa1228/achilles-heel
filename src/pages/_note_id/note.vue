2b5b94a611d4e1ca6e77931e7cb77b12f5ddb639
kawa1228 <sxa.1105bo@gmail.com>
2019-10-21 23:36:06 +0900

    fix: 記事詳細ページでデータを取得

--- src/pages/_note_id/note.vue
+++ src/pages/_note_id/note.vue
@@ -1,3 +1,30 @@
 <template>
-  <p>{{ `記事（ ${$route.params.note_id} )の詳細` }}</p>
+  <article v-if="item">
+    <h1>{{ item.note.title }}</h1>
+    <v-row align="center" justify="center">
+      <v-img
+        :src="item.image.src"
+        :alt="item.image.name"
+        lazy-src="https://placehold.jp/150x150.png"
+        aspect-ratio="1"
+        class="grey lighten-2"
+        max-width="500"
+        max-height="300"
+      ></v-img>
+    </v-row>
+    <p>{{ item.note.body }}</p>
+  </article>
+  <div v-else>記事がありません</div>
 </template>
+
+<script>
+import { db } from '~/plugins/firebase'
+
+export default {
+  async asyncData({ params }) {
+    const docRef = await db.collection('articles').doc(params.note_id)
+    const doc = await docRef.get()
+    return { item: doc.data() }
+  }
+}
+</script>
