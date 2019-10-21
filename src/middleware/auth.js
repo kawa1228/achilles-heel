17ec60d3e0644fbfa3dc75fac0115e1fc8e6cf06
kawa1228 <sxa.1105bo@gmail.com>
2019-10-21 23:10:36 +0900

    fix: user情報があった場合のリダイレクト先を変更

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,8 @@
-import app from '~/plugins/firebase'
+import { app } from '~/plugins/firebase'
 
 export default async function({ store, redirect, route }) {
+  console.log('store', store.state.user)
+  console.log('route', route)
   // 遷移ごとに呼び出されるためstoreに情報があれば処理しない
   if (store.state.user !== null) return
 
@@ -11,16 +13,14 @@ export default async function({ store, redirect, route }) {
   // 未ログインの場合
   if (!user) {
     console.log('ログインしてください')
-    if (route.path !== '/') {
-      return redirect('/admin')
-    }
-  } else {
-    const userInfo = {
-      displayName: user.displayName,
-      email: user.email,
-      photoURL: user.photoURL
-    }
-    await store.dispatch('addUser', userInfo)
-    return redirect('/list')
+    return
   }
+  const userInfo = {
+    displayName: user.displayName,
+    email: user.email,
+    photoURL: user.photoURL,
+    uid: user.uid
+  }
+  await store.dispatch('addUser', userInfo)
+  return redirect(`/${userInfo.uid}/list`)
 }
