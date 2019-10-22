e42f34ea177246d2edbcea86043c928215928ee0
kawa1228 <sxa.1105bo@gmail.com>
2019-10-22 11:16:06 +0900

    layout: 投稿一覧ページのレイアウト修正

--- src/pages/_uid/list.vue
+++ src/pages/_uid/list.vue
@@ -1,46 +1,61 @@
 <template>
-  <v-row v-if="item && item.length">
-    <v-col v-for="(item, i) in item" :key="i" cols="12">
-      <v-card dark nuxt :to="`/${item.noteId}/note`">
-        <div class="d-flex flex-no-wrap justify-space-between">
-          <div>
-            <v-card-title
-              class="headline"
-              v-text="item.note.title"
-            ></v-card-title>
+  <v-layout column justify-center align-center>
+    <v-flex v-if="item && item.length" xs12 sm8 md6>
+      <div v-if="user" class="text-xs-right">
+        <!-- &mdash; George Gershwin -->
+        <em
+          ><small>{{ user.displayName }}投稿一覧</small></em
+        >
+      </div>
+      <hr class="my-3" />
+      <v-row>
+        <v-col v-for="(item, i) in item" :key="i" cols="12">
+          <v-card dark nuxt :to="`/${item.noteId}/note`">
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title
+                  class="headline"
+                  v-text="item.note.title"
+                ></v-card-title>
 
-            <v-card-text v-text="item.note.body" />
-            <v-card-text v-text="item.created_at" />
-          </div>
+                <v-card-text v-text="item.note.body" />
+                <v-card-text v-text="item.created_at" />
+              </div>
 
-          <v-avatar class="ma-3" size="125" tile>
-            <v-img
-              v-if="item.image.src"
-              :src="item.image.src"
-              :alt="item.image.name"
-            >
-              <template v-slot:placeholder>
-                <v-layout fill-height align-center justify-center ma-0>
-                  <!-- 以下のコンポーネントでクルクル回るローディング画像を表示しています。 -->
-                  <v-progress-circular
-                    indeterminate
-                    color="grey lighten-5"
-                  ></v-progress-circular>
-                </v-layout>
-              </template>
-            </v-img>
-          </v-avatar>
-        </div>
-      </v-card>
-    </v-col>
-  </v-row>
-  <div v-else>記事がありません</div>
+              <v-avatar class="ma-3" size="125" tile>
+                <v-img
+                  v-if="item.image.src"
+                  :src="item.image.src"
+                  :alt="item.image.name"
+                >
+                  <template v-slot:placeholder>
+                    <v-layout fill-height align-center justify-center ma-0>
+                      <!-- 以下のコンポーネントでクルクル回るローディング画像を表示しています。 -->
+                      <v-progress-circular
+                        indeterminate
+                        color="grey lighten-5"
+                      ></v-progress-circular>
+                    </v-layout>
+                  </template>
+                </v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-flex>
+    <div v-else>記事がありません</div>
+  </v-layout>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import { db } from '~/plugins/firebase'
 
 export default {
+  computed: {
+    ...mapState(['user'])
+  },
   async asyncData({ params }) {
     const articlesRef = await db.collection('articles')
     const query = articlesRef.where('uid', '==', params.uid)
