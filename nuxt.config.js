a38d9192bb3e1b4006914b6bc89397af3bb21170
kawa1228 <sxa.1105bo@gmail.com>
2019-10-25 19:13:26 +0900

    chore: sitemapの導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,8 +53,14 @@ export default {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/sitemap'
   ],
+  sitemap: {
+    hostname: 'https://my-achilles-heel.firebaseapp.com',
+    lastmod: '2019-10-25',
+    sitemaps: []
+  },
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
